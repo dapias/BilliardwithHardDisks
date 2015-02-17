afee567688e9d7614ce38e40b6bfe61e705168e6
dapias <dandroveros@gmail.com>
2015-02-16 18:43:58 -0600

    Created input_parameters.jl

---
+++ src/input_parameters.jl
@@ -0,0 +1,9 @@
+radius_disk = 1.0
+mass_disk = 1.0
+velocity_disk = 1.0
+Lx1 = 0
+Ly1 = 0
+hole_size = 0.5*radius_disk
+cellforinitialparticle = 1
+radius_puntual_particle = 0.02 #Just for visualization
+mass_particle = 1.0
