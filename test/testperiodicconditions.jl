5e51358b647262963d0b1f5edbef5a2d80b6cf8d
dapias <dandroveros@gmail.com>
2015-03-21 16:02:33 -0600

    Test working.

--- test/testperiodicconditions.jl
+++ test/testperiodicconditions.jl
@@ -60,33 +60,27 @@ function update_y_disk(disk,Ly1,size_y)
   disk.r
 end
 
-cell, particle = HardDiskBilliardModel.create_initial_cell_with_particle(Lx1, Ly1,size_x,size_y,radiusdisk, massdisk, velocitydisk, massparticle, velocityparticle, windowsize)
-disk = cell.disk
-disk.r = [1.5, 1.5]
-disk.v = [0,1.0]
-
-
-
-by1 = Ly1 + disk.radius
-by2 = size_y - disk.radius
+function update_position_disk!(disk,Lx1,Ly1,size_x,size_y,delta_t)
+  move(disk,delta_t)
+  update_y_disk(disk,Ly1,size_y)
+  update_x_disk(disk,Lx1,size_x)
+end
 
 facts("Disk update tests") do
   Lx1 = 5
   Ly1 = 5
-  size_x = 3.                     #Size of the cell in x
+  size_x = 3.
   size_y = 3.
   radiusdisk = 1.0
   massdisk = 1.0
-  disk = HardDiskBilliardModel.Disk([6.5.,6.5],[1.,1.],radiusdisk,massdisk,0)
+  disk = HardDiskBilliardModel.Disk([6.5,6.5],[1.,1.],radiusdisk,massdisk,0)
+  delta_t = 10.0
+  update_position_disk!(disk,Lx1,Ly1,size_x,size_y,delta_t)
+  @fact disk.r => [6.5,6.5]
+  disk = HardDiskBilliardModel.Disk([6.5,6.5],[-1.,-1.],radiusdisk,massdisk,0)
+  delta_t = 10.0
+  update_position_disk!(disk,Lx1,Ly1,size_x,size_y,delta_t)
+  @fact disk.r => [6.5,6.5]
 
-
-
-  D =
-    @fact D.r => [2.0,3.0]
-    @fact D.v => [4.0,6.0]
-    @fact D.radius => 6.0
-    @fact D.mass => 4.0
-    @fact D.numberofcell => 0
-    @fact D.lastcollision => 0
 end
 
