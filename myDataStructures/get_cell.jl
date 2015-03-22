9fadf18922f0f3bc4174b78e5ab91010ec621681
dapias <dandroveros@gmail.com>
2015-03-21 19:19:25 -0600

    Add changes to test with different values of n

--- myDataStructures/get_cell.jl
+++ myDataStructures/get_cell.jl
@@ -8,7 +8,7 @@ include("parameters.jl")
 function get_cell(q::Deque, numberofcell::Int)
     @assert -1023 <= numberofcell <= 1023
     if numberofcell < 0
-        q.head.data[end+numberofcell]
+        q.head.data[end+numberofcell+1]
     else
         q.rear.data[numberofcell+1]
     end
@@ -18,7 +18,17 @@ board, particle = create_board_with_particle(Lx1, Ly1,size_x,size_y,radiusdisk,m
                            massparticle, velocityparticle, windowsize);
 
 board = board.cells
+cell = get_cell(board,0)
+for i in 1:10
+    cell = HardDiskBilliardModel.create_new_left_cell(cell, particle)
+    unshift!(board, cell)
+end
+
+cell = get_cell(board,0)
+for i in 1:10
+    cell = HardDiskBilliardModel.create_new_right_cell(cell, particle)
+    push!(board,cell)
+end
 
 
 
-get_cell(board,0)
