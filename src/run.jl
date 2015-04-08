34a00b2d5ddc5d303dc31374151ae4a037ada019
David P. Sanders <dpsanders@gmail.com>
2015-04-08 12:56:12 -0500

    Cambiamos run.jl para tener función run() y aceptar argumentos de la línea de comandos a través de ARGS

--- src/run.jl
+++ src/run.jl
@@ -7,54 +7,59 @@
 
 push!(LOAD_PATH,"./")
 using HardDiskBilliardSimulation
-using Visual
+#using Visual
 using Compat  ## To handle versions less than 0.4
 using DataStructures
 
 #srand(1234)
-visual = false
 
-# To change a parameter, type: parameters[:nameofsymbol] = valueyouwanttoset
-parameters = @compat Dict(:t_initial => 0,
-                  :t_max => 100,
-                  :radiusdisk => 1.0,
-                  :massdisk => 1.0,
-                  :velocitydisk => 1.0,
-                  :Lx1 => 0,                         #x position of the first cell
-                  :Ly1 => 0,                         #y position of the first cell
-                  :windowsize => 0.5,
-                  :massparticle => 1.0,
-                  :size_x => 3.,                     #Size of the cell in x
-                  :size_y => 3.,                     #Size of the cell in y
-                  :velocityparticle => 1.0
-                  )
+function run(t_final = 100)
+
+    visual = false
+
+    # To change a parameter, type: parameters[:nameofsymbol] = valueyouwanttoset
+    parameters = @compat Dict(:t_initial => 0,
+                      :t_max => 100,
+                      :radiusdisk => 1.0,
+                      :massdisk => 1.0,
+                      :velocitydisk => 1.0,
+                      :Lx1 => 0,                         #x position of the first cell
+                      :Ly1 => 0,                         #y position of the first cell
+                      :windowsize => 0.5,
+                      :massparticle => 1.0,
+                      :size_x => 3.,                     #Size of the cell in x
+                      :size_y => 3.,                     #Size of the cell in y
+                      :velocityparticle => 1.0
+                      )
 
 
-#parameters[:t_max] = 1000
+    #parameters[:t_max] = 1000
 
-if visual
-    radiustovisualizeparticle = 0.02
-    sim = animatedsimulation(;parameters...);
-    @time visualize(sim, radiustovisualizeparticle);
-    delta_e_max, = findmax(sim[end-1])
-    delta_e_min, = findmin(sim[end - 1])
-    println("Delta_E_max, Delta_E_min = $(delta_e_max),$(delta_e_min)")
-#     time = sim[5]
-#     nofevents = length(time)
-#     println("# of events: $nofevents")
-else
-    parameters[:t_max] = 100
-    @time sim = simulation(;parameters...);
-    board = sim[1]
-    left = back(board.cells).numberofcell
-    right = front(board.cells).numberofcell
-    println("Left-cell, Right-cell: $left,$right")
-    time = sim[4]
-    nofevents = length(time)
-    println("# of events: $nofevents")
+    if visual
+        radiustovisualizeparticle = 0.02
+        sim = animatedsimulation(;parameters...);
+        @time visualize(sim, radiustovisualizeparticle);
+        delta_e_max, = findmax(sim[end-1])
+        delta_e_min, = findmin(sim[end - 1])
+        println("Delta_E_max, Delta_E_min = $(delta_e_max),$(delta_e_min)")
+    #     time = sim[5]
+    #     nofevents = length(time)
+    #     println("# of events: $nofevents")
+    else
+        parameters[:t_max] = t_final
+        elapsed_time = @elapsed sim = simulation(;parameters...);
+        board = sim[1]
+        left = back(board.cells).numberofcell
+        right = front(board.cells).numberofcell
+        println("Left-cell, Right-cell: $left,$right")
+        time = sim[4]
+        nofevents = length(time)
+        println("# of events: $nofevents")
+    end
+
+    elapsed_time
+
 end
-
-
 
 # if (ARGS)[1] != 0
 #     if ARGS[1] == "true"
@@ -66,3 +71,13 @@ end
 
 #visualize_localenergy(sim);
 #just for git
+
+try
+    t_final = float(ARGS[1])
+    elapsed_time = run(t_final)
+    println("# Tomé $elapsed_time segundos")
+
+catch
+    println("Sintaxis: julia run.jl t_final")
+
+end
