d685287a7ca12aa6fc6379fc128312ac50fa58a2
dapias <dandroveros@gmail.com>
2015-04-09 12:30:11 -0500

    Add a comment to run optimally the file

--- residence_time/runall.jl
+++ residence_time/runall.jl
@@ -1,3 +1,5 @@
+#To run the file the first time it is needed to create the empty folders "HDF5" and "images" in this path.
+
 nameoffile = "100,000realizaciones"  #It will throw an error if the nameoffile is already exists
 nofrealizations = 100000
 nofbars = 20 #For the histogram
